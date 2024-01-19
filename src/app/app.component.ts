e12606d20ffeb920fc837d037b53346c83e3ed3d
VitorB2002 <vitorb.santos2002@gmail.com>
2024-01-19 12:13:04 -0300

    Add(header): adicionado estrutura para a header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent,
+    HeroComponent,
+    ContentComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
